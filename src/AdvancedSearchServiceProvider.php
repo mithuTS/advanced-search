5d199f21ef18c2675028b58ebe1bea5a054f77f9
Mithu A Quayium <cemithu06@gmail.com>
2018-02-21 13:09:03 +0600

    Initial commit

---
+++ src/AdvancedSearchServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace mithuTS\AdvancedSearch;
+use Illuminate\Support\ServiceProvider;
+
+class AdvancedSearchServiceProvider extends ServiceProvider{
+
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+}
